52ab42e2b7eda9822787719d85a156f5c84c6e2c
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2023-12-10 23:34:50 -0500

    Finished Day 8 Part 2

--- AdventOfCode2023/Day8/Day8.cs
+++ AdventOfCode2023/Day8/Day8.cs
@@ -2,34 +2,75 @@ using System.Text.RegularExpressions;
 
 partial class Day8
 {
+    string _instructions;
+    Dictionary<string, Node> _nodeMap;
+
     public async Task Run()
     {
         var input = await File.ReadAllLinesAsync($"{GetType()}/input.txt");
 
         Console.WriteLine("Running Day 8 - Part 1");
 
-        var instructions = input[0];
+        _instructions = input[0];
+        _nodeMap = input.Skip(2).Select(Node.ParseNode).ToDictionary(n => n.Id);
 
-        var nodes = input.Skip(2).Select(Node.ParseNode).ToDictionary(n => n.Id);
+        var steps = CountStepsToEnd(_nodeMap["AAA"], n => n.Id == "ZZZ");
+        Console.WriteLine($"Steps to 'ZZZ' = {steps}");
+        
+        Console.WriteLine("Running Day 8 - Part 2");
 
-        var curr = nodes["AAA"];
-        int steps = 0;
+        // Through experimentation, I learned that:
+        // - There are six start nodes and six end nodes.
+        //   - starts: AAA, GQA, GVA, HBA, NVA, XCA
+        //   - ends:   ZZZ, TKZ, KJZ, JLZ, HVZ, LLZ
+        // - Following the instructions from each start node ends in a loop with the same end node.
+        //   - AAA->ZZZ, GQA->TKZ, GVA->KJZ, HBA->JLZ, NVA->HVZ, and XCA->LLZ
+        // - The time to reach the end the first time is the same as the rest. (WOW!)
+        //   - 18727, 22411, 16271, 14429, 20569, 24253
+        // Therefore:
+        // - This is a Least Common Multiple problem.
+
+        var startNodes = _nodeMap.Values.Where(n => n.Id[2] == 'A').ToList();
+        var stepCounts = startNodes.Select(n => CountStepsToEnd(n, c => c.Id[2] == 'Z')).ToList();
+
+        var ghostSteps = stepCounts.Aggregate(LeastCommonMultiple);
+        
+        Console.WriteLine($"Steps to concurrent nodes ending with 'Z' = {ghostSteps}");
+    }
+
+    private long CountStepsToEnd(Node start, Predicate<Node> endCheck)
+    {
+        long steps = 0;
         int cursor = 0;
-        while (curr.Id != "ZZZ")
+        var curr = start;
+        while (!endCheck(curr))
         {
-            curr = instructions[cursor] switch 
+            curr = _instructions[cursor] switch 
             {
-                'L' => nodes[curr.Left],
-                'R' => nodes[curr.Right],
+                'L' => _nodeMap[curr.Left],
+                'R' => _nodeMap[curr.Right],
                 _ => curr
             };
             ++steps;
-            cursor = (cursor + 1) % instructions.Length;
+            cursor = (cursor + 1) % _instructions.Length;
         }
+        return steps;
+    }
 
-        Console.WriteLine($"Steps to 'ZZZ' = {steps}");
+    private long GreatestCommonFactor(long a, long b)
+    {
+        while (b != 0)
+        {
+            long tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
 
-        Console.WriteLine("Running Day 8 - Part 2");
+    private long LeastCommonMultiple(long a, long b)
+    {
+        return a / GreatestCommonFactor(a, b) * b;
     }
 
     partial record Node
