4c6b32d26a0591dc64cdd0fd044a31d3c749252c
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2023-12-16 10:08:48 -0500

    Finished Day 11 Part 2

--- AdventOfCode2023/Day11/Day11.cs
+++ AdventOfCode2023/Day11/Day11.cs
@@ -1,40 +1,54 @@
 class Day11
 {
+    private readonly List<(int x, int y)> galaxies = [];
+    private HashSet<int> galaxyRows;
+    private HashSet<int> galaxyCols;
+
     public async Task Run()
     {
         var input = await File.ReadAllLinesAsync($"{GetType()}/input.txt");
 
         Console.WriteLine("Running Day 11 - Part 1");
 
-        List<(int x, int y)> galaxies = [];
         for (int y = 0; y < input.Length; ++y)
             for (int x = 0; x < input[y].Length; ++x)
                 if (input[y][x] == '#')
                     galaxies.Add((x, y));
 
-        HashSet<int> galaxyRows = galaxies.Select(g => g.y).ToHashSet();
-        HashSet<int> galaxyCols = galaxies.Select(g => g.x).ToHashSet();
+        galaxyRows = galaxies.Select(g => g.y).ToHashSet();
+        galaxyCols = galaxies.Select(g => g.x).ToHashSet();
 
-        int lengthSum = 0;
+        Console.WriteLine($"Sum of shortest path lengths = {CalcLengthSum(2)}");
+
+        Console.WriteLine("Running Day 11 - Part 2");
+
+        Console.WriteLine($"Sum of shortest path lengths = {CalcLengthSum(1000000)}");
+    }
+
+    private long CalcLengthSum(int age)
+    {
+        long lengthSum = 0;
         for (int i = 0; i < galaxies.Count - 1; ++i)
             for (int j = i + 1; j < galaxies.Count; ++j)
             {
                 var (ax, ay) = galaxies[i];
                 var (bx, by) = galaxies[j];
-                LowHigh(ref ax, ref bx);
-                LowHigh(ref ay, ref by);
-                int dx = bx - ax;
-                int dy = by - ay;
-
-                dx += Enumerable.Range(ax, dx).Where(x => !galaxyCols.Contains(x)).Count();
-                dy += Enumerable.Range(ay, dy).Where(x => !galaxyRows.Contains(x)).Count();
-
-                lengthSum += dx + dy;             
+                lengthSum += CalcLength(ax, ay, bx, by, age);         
             }
+        return lengthSum;
+    }
 
-        Console.WriteLine($"Sum of shortest path lengths = {lengthSum}");
+    private int CalcLength(int ax, int ay, int bx, int by, int age)
+    {
+        LowHigh(ref ax, ref bx);
+        LowHigh(ref ay, ref by);
+        int dx = bx - ax;
+        int dy = by - ay;
 
-        Console.WriteLine("Running Day 11 - Part 2");
+        dx += Enumerable.Range(ax, dx).Count(x => !galaxyCols.Contains(x)) * (age - 1);
+        dy += Enumerable.Range(ay, dy).Count(x => !galaxyRows.Contains(x)) * (age - 1);
+
+        return dx + dy;
     }
 
     private static void LowHigh(ref int a, ref int b)
