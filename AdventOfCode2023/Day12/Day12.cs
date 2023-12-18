095e832fd88485870a680d2235461c79db19d2c8
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2023-12-18 08:36:55 -0500

    Finished Day 12 Part 2 (doesn't complete execution)

--- AdventOfCode2023/Day12/Day12.cs
+++ AdventOfCode2023/Day12/Day12.cs
@@ -6,94 +6,57 @@ class Day12
 
         Console.WriteLine("Running Day 12 - Part 1");
 
-        int matchCountSum = 0;
+        long matchCountSum = 0;
         foreach (var line in input)
         {
             var parts = line.Split(' ');
             List<char> row = [..parts[0]];
             var groups = parts[1].Split(',').Select(int.Parse).ToList();
 
-            int matches = CountMatches(row, groups);
-            matchCountSum += matches;
+            matchCountSum += CountMatches(row, groups);
         }
 
         Console.WriteLine($"Valid Arrangements = {matchCountSum}");
 
         Console.WriteLine("Running Day 12 - Part 2");
+
+        matchCountSum = 0;
+        Parallel.For(0, input.Length, i =>
+        {
+            string line = input[i];
+            var parts = line.Split(' ');
+            List<char> row = [..parts[0], '?', ..parts[0], '?', ..parts[0], '?', ..parts[0], '?', ..parts[0]];
+            var groups = parts[1].Split(',').Select(int.Parse).ToList();
+            groups = [..groups, ..groups, ..groups, ..groups, ..groups];
+
+            long matches = CountMatches(row, groups);
+            Interlocked.Add(ref matchCountSum, matches);
+        });
+
+        Console.WriteLine($"Valid Arrangements = {matchCountSum}");
     }
 
-    private static int CountMatches(List<char> row, List<int> groups, int i = 0, int currentGroupSize = 0)
+    private static long CountMatches(List<char> row, List<int> groups, int i = 0, int currentGroupSize = 0)
     {
         if (i == row.Count)
-        {
-            if (groups.Count == 1 && currentGroupSize == groups[0])
+            return (groups.Count, currentGroupSize) switch
             {
-                return 1;
-            }
-            else if (groups.Count == 0 && currentGroupSize == 0)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }
-        }
+                (0, 0) => 1,
+                (1, _) when currentGroupSize == groups[0] => 1,
+                _ => 0
+            };
 
-        if (row[i] == '.')
+        return (row[i], groups.Count, currentGroupSize) switch
         {
-            if (groups.Count == 0 && currentGroupSize == 0)
-            {
-                // valid so far, keep going
-                return CountMatches(row, groups, i + 1, currentGroupSize);
-            }
-            else
-            {
-                if (currentGroupSize == 0)
-                {
-                    // keep moving
-                    return CountMatches(row, groups, i + 1, currentGroupSize);
-                }
-                // See if the previous group matches the size of the first unmatched group
-                if (groups[0] == currentGroupSize)
-                {
-                    return CountMatches(row, groups[1..], i + 1, 0);
-                }
-                else
-                {
-                    // This means groups[0] was unsatisfied.
-                    return 0;
-                }
-            }
-        }
-        else if (row[i] == '#')
-        {
-            if (groups.Count == 0)
-            {
-                // no groups left but extra damaged spring.
-                return 0;
-            }
-            // See if the current group's size is <= the first unmatched group
-
-            else if (currentGroupSize < groups[0])
-            {
-                // Then we can add one and still be a match. Try it.
-                return CountMatches(row, groups, i + 1, currentGroupSize + 1);
-            }
-            else
-            {
-                // we make too big a group to match.
-                return 0;
-            }
-        }
-        else // if (row[i] == '?')
-        {
-            // Try a #
-            List<char> testDamaged = [..row[0..i] , '#', ..row[(i + 1)..]];
-            // Try a .
-            List<char> testOperational = [..row[0..i], '.', ..row[(i + 1)..]];
-
-            return CountMatches(testDamaged, groups, i, currentGroupSize) + CountMatches(testOperational, groups, i, currentGroupSize);
-        }
+            ('.', _, 0) => CountMatches(row, groups, i + 1, currentGroupSize),
+            ('.', > 0, _) when groups[0] == currentGroupSize => CountMatches(row, groups[1..], i + 1, 0),
+            ('.', _, _) => 0,
+            ('#', 0, _) => 0,
+            ('#', _, _) when currentGroupSize < groups[0] => CountMatches(row, groups, i + 1, currentGroupSize + 1),
+            ('#', _, _) => 0,
+            (_, _, _) => 
+                CountMatches([..row[0..i] , '#', ..row[(i + 1)..]], groups, i, currentGroupSize) + 
+                CountMatches([..row[0..i], '.', ..row[(i + 1)..]], groups, i, currentGroupSize)
+        };
     }
 }
