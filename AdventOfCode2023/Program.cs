8d9b1adfed1fdb1aaa2e63ff68e4460595abfe9e
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2023-12-29 20:46:24 -0500

    Added support for running a specific day

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -7,13 +7,17 @@ Console.WriteLine("\n🎅🎅🎅🎅🎅 ADVENT OF CODE 2023 🎅🎅🎅🎅�
 var dayTimer = new Stopwatch();
 var yearTimer = new Stopwatch();
 
-static async Task RunDay(Stopwatch w, int day)
+const bool RUN_ONE_DAY = true; // defaults to running newest day.
+const int DAY_TO_RUN = 15; // When running one day, optional specification of which day to run.
+
+static async Task<bool> RunDay(Stopwatch w, int day)
 {
+    if (DAY_TO_RUN != -1 && DAY_TO_RUN != day)
+        return false;
+
     Type? dayType = Type.GetType($"Day{day}");
     if (dayType == null)
-    {
-        return;
-    }
+        return false;
 
     MethodInfo runMethod = dayType.GetMethod("Run")!;
     var dayInstance = Activator.CreateInstance(dayType);
@@ -29,12 +33,22 @@ static async Task RunDay(Stopwatch w, int day)
     
     w.Stop();
     Console.WriteLine($"Day {day} Time = {w.Elapsed}");
+
+    return true;
 }
 
 yearTimer.Start();
 
-for (int i = 1; i <=25; ++i) {
-    await RunDay(dayTimer, i);
+if (RUN_ONE_DAY)
+{
+    for (int i = 25; i >= 1; --i)
+        if (await RunDay(dayTimer, i))
+            break;
+}
+else
+{
+    for (int j = 1; j <= 25; ++j)
+        await RunDay(dayTimer, j);
 }
 
 yearTimer.Stop();
