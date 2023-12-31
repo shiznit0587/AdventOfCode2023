c4db0625dac42e46814cc4d22e6bba1d0675a0fe
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2023-12-30 19:59:28 -0500

    Finished Day 15 Part 1

--- AdventOfCode2023/Day15/Day15.cs
+++ AdventOfCode2023/Day15/Day15.cs
@@ -9,8 +9,71 @@ class Day15
         Console.WriteLine($"Hash sums = {input.Select(RunHash).Sum()}");
 
         Console.WriteLine("Running Day 15 - Part 2");
+
+        var boxes = Enumerable.Range(0, 256).Select(i => new List<Lens>()).ToList();
+
+        foreach (var instr in input.Select(InitStep.Parse))
+        {
+            var box = boxes[instr.Box];
+            var lens = box.Find(l => l.Label == instr.Label);
+
+            switch (instr.Removal, lens)
+            {
+                case (true, not null):
+                    box.Remove(lens);
+                    break;
+                case (false, not null):
+                    lens.FocalLength = instr.FocalLength;
+                    break;
+                case (false, null):
+                    box.Add(new Lens { Label = instr.Label, FocalLength = instr.FocalLength });
+                    break;
+            }
+        }
+
+        int totalFocusingPower = 0;
+        for (int i = 0; i < boxes.Count; ++i)
+            for (int j = 0; j < boxes[i].Count; ++j)
+                totalFocusingPower += (i + 1) * (j + 1) * boxes[i][j].FocalLength;
+
+        Console.WriteLine($"Total focusing power = {totalFocusingPower}");
     }
 
-    int RunHash(string str) =>
+    static int RunHash(string str) =>
         str.Aggregate(0, (h, c) => (h + c) * 17 % 256);
+
+    class Lens
+    {
+        public required string Label;
+        public int FocalLength;
+    }
+
+    class InitStep
+    {
+        public string Label;
+        public int Box;
+        public int FocalLength;
+        public bool Removal;
+
+        public InitStep(string label)
+        {
+            Label = label;
+            Box = RunHash(Label);
+        }
+
+        public InitStep(string label, int focalLength)
+            : this(label)
+        {
+            FocalLength = focalLength;
+        }
+
+        public static InitStep Parse(string instr)
+        {
+            if (instr[^1] == '-')
+                return new InitStep(instr[0..^1]) { Removal = true };
+
+            var parts = instr.Split('=');
+            return new InitStep(parts[0], int.Parse(parts[1]));
+        }
+    }
 }
